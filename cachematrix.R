16edbf9753ab70e3381345f9b9678835f316159a
physicist-codergit config --global user.email thestrongestavenger60@gmail.comgit config --listgit config --global user.name physicist-coder <thestrongestavenger60@gmail.com>
2020-06-03 20:48:19 +0530

    Code for the assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## is calculated and printed out, if inverse is already present it is printed out
+## this is a function which is used to get (or) set the cache matrix and inverse of the matrix
+makeCacheMatrix <- function(a = matrix()){
+    #initialzing
+    inbuilt<-NULL;
+    set_matrix<-function(y){
+        a<<-y;
+        inbuilt<<-NULL;
+    }
+    #getting the matrix
+    get_matrix=function() a
+    #setting the matrix inverse to the cache
+    set_inv=function(inv) inbuilt<<-inv
+    #getting the cache
+    get_inv=function() inbuilt
+    list(set_matrix=set_matrix,get_matrix=get_matrix,set_inv=set_inv,get_inv=get_inv)
 }
 
+#this function checks if the given matrix is already present or not
+#if present it fetches data from the previous matrix
+#if not present it calculates the inverse and adds that to the cache
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve<-function(x,...){
+    #checks if the matrix inverse is already present, if yes print it out
+    inverse_matrix=x$get_inv()
+    if(!is.null(inverse_matrix)){
+        return(inverse_matrix)
+    }
+    #if not solves for the matrix, prints it and stores it in the cache
+    matrix<-x$get_matrix()
+    inverse_matrix=solve(matrix)
+    x$set_inv(inverse_matrix)
+    inverse_matrix
 }
